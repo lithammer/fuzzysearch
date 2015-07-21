5c2779be903c1ab45554aa4f8dbfbba850c6ee23
Peter Renström <renstrom.peter@gmail.com>
2015-07-21 23:26:02 +0200

    Add ranked fuzzy search functions.
    Issue #1.

--- fuzzy/fuzzy_test.go
+++ fuzzy/fuzzy_test.go
@@ -9,21 +9,69 @@ var fuzzyTests = []struct {
 	needle   string
 	haystack string
 	wanted   bool
+	rank     int
 }{
-	{"twl", "cartwheel", true},
-	{"cart", "cartwheel", true},
-	{"cw", "cartwheel", true},
-	{"ee", "cartwheel", true},
-	{"art", "cartwheel", true},
-	{"eeel", "cartwheel", false},
-	{"dog", "cartwheel", false},
+	{"twl", "cartwheel", true, 6},
+	{"cart", "cartwheel", true, 5},
+	{"cw", "cartwheel", true, 7},
+	{"ee", "cartwheel", true, 7},
+	{"art", "cartwheel", true, 6},
+	{"eeel", "cartwheel", false, -1},
+	{"dog", "cartwheel", false, -1},
 }
 
-func TestFuzzyMatch(t *testing.T) {
-	for _, v := range fuzzyTests {
-		match := Search(v.needle, v.haystack)
-		if match != v.wanted {
-			t.Errorf("%s in %s expected match to be %t, got %t", v.needle, v.haystack, v.wanted, match)
+func TestFuzzySearch(t *testing.T) {
+	for _, val := range fuzzyTests {
+		match := Search(val.needle, val.haystack)
+		if match != val.wanted {
+			t.Errorf("%s in %s expected match to be %t, got %t",
+				val.needle, val.haystack, val.wanted, match)
+		}
+	}
+}
+
+func TestFuzzySearchMany(t *testing.T) {
+	haystack := []string{"cartwheel", "foobar", "wheel", "baz"}
+	wanted := []string{"cartwheel", "wheel"}
+
+	matches := SearchMany("whl", haystack)
+
+	if len(matches) != len(wanted) {
+		t.Errorf("expected %s, got %s", wanted, matches)
+	}
+
+	for i := range wanted {
+		if wanted[i] != matches[i] {
+			t.Errorf("expected %s, got %s", wanted, matches)
+		}
+	}
+}
+
+func TestRankSearch(t *testing.T) {
+	for _, val := range fuzzyTests {
+		rank := RankSearch(val.needle, val.haystack)
+		if rank != val.rank {
+			t.Errorf("expected ranking %d, got %d", val.rank, rank)
+		}
+	}
+}
+
+func TestRankSearchMany(t *testing.T) {
+	haystack := []string{"cartwheel", "foobar", "wheel", "baz"}
+	wanted := []Rank{
+		{"cartwheel", 6},
+		{"wheel", 2},
+	}
+
+	ranks := RankSearchMany("whl", haystack)
+
+	if len(ranks) != len(wanted) {
+		t.Errorf("expected %+v, got %+v", wanted, ranks)
+	}
+
+	for i := range wanted {
+		if wanted[i] != ranks[i] {
+			t.Errorf("expected %+v, got %+v", wanted, ranks)
 		}
 	}
 }
