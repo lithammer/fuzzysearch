3f9dd27f9987775eddd9938308e7850fb3627b29
rharriso <rtharrison86@gmail.com>
2015-08-06 21:50:39 -0400

    use same size tests on levenstein and fuzzy

--- fuzzy/fuzzy_test.go
+++ fuzzy/fuzzy_test.go
@@ -12,6 +12,7 @@ var fuzzyTests = []struct {
 	wanted bool
 	rank   int
 }{
+	{"stakes", "The only really major change from the <em>status quo ante</em> resulting from the War of Austrian Succession was Austria’s loss of Silesia to Prussia. As a consequence, Frederick increased the number of his subjects by over 50 percent and made Prussia the first new entrant to the ranks of the Great Powers since the Peace of Westphalia in 1648. Frederick—who by this point was already being called “the Great” by his admirers—had demonstrated that as a political instrument war could accomplish major objectives even in an age of “limited war,” when sociopolitical constraints as well as military ones tended to make wars relatively indecisive and profitless. Still, the Prussian king was able to make such substantial gains only because his superior soldiers and his increasingly adept generalship enabled him to win five consecutive battles against the Austrians. Even that impressive military record, however, bought Prussia only an insecure conquest and a cold and temporary peace. Maria Theresa remained determined to recover Silesia, and worked tirelessly to improve her army and adjust the political situation to make that possible. The result, the subject of chapter 9, was a renewed effort to undo Frederick’s gains, a war that would push Austria, France, Britain, and Prussia towards a style of warfare employing greater resources and involving higher political stakes.", true, 1374},
 	{"twl", "cartwheel", true, 6},
 	{"cart", "cartwheel", true, 5},
 	{"cw", "cartwheel", true, 7},
@@ -95,14 +96,16 @@ func TestSortingRanks(t *testing.T) {
 }
 
 func BenchmarkMatch(b *testing.B) {
+	ft := fuzzyTests[0]
 	for i := 0; i < b.N; i++ {
-		Match("kitten", "sitting")
+		Match(ft.source, ft.target)
 	}
 }
 
 func BenchmarkRankMatch(b *testing.B) {
+	ft := fuzzyTests[0]
 	for i := 0; i < b.N; i++ {
-		RankMatch("kitten", "sitting")
+		RankMatch(ft.source, ft.target)
 	}
 }
 
