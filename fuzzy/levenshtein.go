a928105e0a7da63efcb91a3e4beff9f02a1ae2eb
Peter Lithammer <peter.lithammer@gmail.com>
2023-04-28 09:43:29 +0200

    perf: use `CMOV` instruction to speed up `min()`
    This increases performance of `min()` by 1-18% depending on scenario.
    
    ```
                                  │   old.txt    │               new.txt               │
                                  │    sec/op    │   sec/op     vs base                │
    LevenshteinDistance-8            61.08n ± 0%   56.71n ± 0%   -7.15% (p=0.000 n=10)
    LevenshteinDistanceBigLate-8    11.239µ ± 0%   9.230µ ± 0%  -17.88% (p=0.000 n=10)
    LevenshteinDistanceBigEarly-8    9.424µ ± 0%   9.295µ ± 0%   -1.37% (p=0.000 n=10)
    geomean                          1.863µ        1.694µ        -9.06%
    ```
    
    Fixes #50

--- fuzzy/levenshtein.go
+++ fuzzy/levenshtein.go
@@ -33,11 +33,13 @@ func LevenshteinDistance(s, t string) int {
 	return column[len(r1)]
 }
 
-func min(a, b, c int) int {
-	if a < b && a < c {
+func min2(a, b int) int {
+	if a < b {
 		return a
-	} else if b < c {
-		return b
 	}
-	return c
+	return b
+}
+
+func min(a, b, c int) int {
+	return min2(min2(a, b), c)
 }
